5e2b794085c6a8d7547d2450ab6ef6fd3b3e4159
Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>
2025-05-28 15:27:45 +0300

    Remove "/dev/disk/by-id/" from flashing
    Signed-off-by: Ville-Pekka Juntunen <ville-pekka.juntunen@unikie.com>

--- ghaf-hw-test.groovy
+++ ghaf-hw-test.groovy
@@ -197,18 +197,18 @@ pipeline {
             SECTOR = 512
             MIB_TO_SECTORS = 20480
             // Disk size in 512-byte sectors
-            SECTORS = sh(script: "/run/wrappers/bin/sudo blockdev --getsz /dev/disk/by-id/${dev}", returnStdout: true).trim()
+            SECTORS = sh(script: "/run/wrappers/bin/sudo blockdev --getsz ${dev}", returnStdout: true).trim()
             // Unmount possible mounted filesystems
-            sh "sync; /run/wrappers/bin/sudo umount -q /dev/disk/by-id/${dev}* || true"
+            sh "sync; /run/wrappers/bin/sudo umount -q ${dev}* || true"
             // Wipe first 10MiB of disk
-            sh "/run/wrappers/bin/sudo dd if=/dev/zero of=/dev/disk/by-id/${dev} bs=${SECTOR} count=${MIB_TO_SECTORS} conv=fsync status=none"
+            sh "/run/wrappers/bin/sudo dd if=/dev/zero of=${dev} bs=${SECTOR} count=${MIB_TO_SECTORS} conv=fsync status=none"
             // Wipe last 10MiB of disk
-            sh "/run/wrappers/bin/sudo dd if=/dev/zero of=/dev/disk/by-id/${dev} bs=${SECTOR} count=${MIB_TO_SECTORS} seek=\$(( ${SECTORS} - ${MIB_TO_SECTORS} )) conv=fsync status=none"
+            sh "/run/wrappers/bin/sudo dd if=/dev/zero of=${dev} bs=${SECTOR} count=${MIB_TO_SECTORS} seek=\$(( ${SECTORS} - ${MIB_TO_SECTORS} )) conv=fsync status=none"
           }
           // Write the image
           img_relpath = run_cmd("find ${TMP_IMG_DIR} -type f -print -quit | grep .")
           println "Using image '$img_relpath'"
-          sh "/run/wrappers/bin/sudo dd if=${img_relpath} of=/dev/disk/by-id/${dev} bs=1M status=progress conv=fsync"
+          sh "/run/wrappers/bin/sudo dd if=${img_relpath} of=${dev} bs=1M status=progress conv=fsync"
           // Unmount
           sh "${unmount_cmd}"
         }
